847b6d26046b73ca35908a305105da31ac668ab7
Jeffrey Toppings <jeffrey.toppings@usask.ca>
2019-07-06 11:31:35 -0600

    Revert "Create nav bar"
    This reverts commit 51d21933

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import {MainContainerComponent} from './main-container/main-container.component';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    NavBarComponent,
-    MainContainerComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule
