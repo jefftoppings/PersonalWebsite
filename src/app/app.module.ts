aff1f8fe195a63216409d03fc983d3b9d3aa0ed2
Jeffrey Toppings <jeffrey.toppings@usask.ca>
2019-07-28 13:13:44 -0600

    change background to dark gray

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
+import {MainContentComponent} from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
