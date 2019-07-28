3c12768850623de7c791ecd76ed1055dd7e5a418
Jeffrey Toppings <jeffrey.toppings@usask.ca>
2019-07-28 14:59:35 -0600

    move personal projects to own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {MainContentComponent} from './main-content/main-content.component';
+import {PersonalProjectsComponent} from './personal-projects/personal-projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainContentComponent
+    MainContentComponent,
+    PersonalProjectsComponent
   ],
   imports: [
     BrowserModule
